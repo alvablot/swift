f6643fb762b8f00fa1213816ffe8deb277b3468e
Petter Karlsson <petter.karlsson@cmeducations.se>
2024-01-24 13:57:12 +0100

    Initial Commit

---
+++ petter-test/petter_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  petter_testApp.swift
+//  petter-test
+//
+//  Created by Petter Kalrsson on 2024-01-24.
+//
+
+import SwiftUI
+
+@main
+struct petter_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
